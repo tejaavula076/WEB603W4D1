9b8fc11a9471a2f9d70e598a6d81766dd4eaab26
= <avula.usedu1@gmail.com>
2026-03-23 18:22:01 -0500

    demo3 done

--- demo3/on-click.js
+++ demo3/on-click.js
@@ -6,61 +6,44 @@ class OnClickElements extends React.Component {
   }
 
   //add handler
+  handleClick(event) {
+    alert(`hey! You clicked: ${event.target.id}`);
+
+  }
 
   render() {
     // TODO: Your task is to add onClick listeners to each of the elements within the container-div <div>
     return (
       <div className="container-div">
-        <div id="div-element" >
+        <div id="div-element" onClick={(e) => this.handleClick(e)}>
           I am DIV
         </div>
 
-        <span id="span-element" >
-          I am SPAN
-        </span>
+        <span id="span-element">I am SPAN</span>
         <br></br>
 
-        <button id="button-element" >
-          I am Button
-        </button>
+        <button id="button-element">I am Button</button>
         <br></br>
 
-        <a id="link-element" href="" >
+        <a id="link-element" href="">
           I am LINK
         </a>
 
-        <div
-          id="div-element-2"
-          className="button"
-          
-        >
+        <div id="div-element-2" className="button"  onClick={(e) => this.handleClick(e)}>
           I am DIV
         </div>
 
-        <span
-          id="span-element-2"
-          className="button"
-          
-        >
+        <span id="span-element-2" className="button"  onClick={(e) => this.handleClick(e)}>
           I am SPAN
         </span>
         <br></br>
 
-        <button
-          id="button-element-2"
-          className="button"
-          
-        >
+        <button id="button-element-2" className="button"  onClick={(e) => this.handleClick(e)}>
           I am Button
         </button>
         <br></br>
 
-        <a
-          id="link-element-2"
-          className="button"
-          href=""
-          
-        >
+        <a id="link-element-2" className="button" href=""  onClick={(e) => this.handleClick(e)}>
           I am LINK
         </a>
       </div>
@@ -70,5 +53,5 @@ class OnClickElements extends React.Component {
 
 ReactDOM.render(
   React.createElement(OnClickElements),
-  document.getElementById("root")
+  document.getElementById("root"),
 );
